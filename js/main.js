29e98baf42cf5be8ae485afb44fb7367fb8e7087
Ristyslav Kinash <83139382+Rostik2202@users.noreply.github.com>
2023-07-01 20:22:07 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+new Swiper('.paralax__slider', {
+    direction: 'vertical',
+    speed: 2000,
+    mousewheel: {
+        enabled: true,
+        sensitivity: 2.5,
+    },
+    spaceBetween: 15,
+    parallax: true,
+})
